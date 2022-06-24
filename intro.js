730a15298f555189006cd1bc6418aaf218c3b726
claudia <105383248+claudinblack@users.noreply.github.com>
2022-06-24 02:12:28 +0100

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,10 @@
+
+    var Name = 'Orji Ngozi Claudia';
+     var height = '1.55m';
+    var country = 'Nigeria';
+
+
+    document.write(Name + "<br>" + height + "<br>" + country);
+ alert(Name + "\n" + height + '\n' + country);   
+
+    
